0d49e3bb7b1b14987cf92f9178524d952d5e1fa0
JoaoPNuneSilva <joaopedronunesdasilva2@gmail.com>
2023-02-27 21:52:50 -0300

    aula

--- joao.py
+++ joao.py
@@ -3,5 +3,7 @@ print("Eu sou a vingança")
 print(22, 33, 44)
 print(22, 33, 44, sep="-")
 print("Eu sou a vingança", 22, 33, 44, sep="-")
-print (30 == 30)
-print (30 == 31)
+print (30 == 30) #false
+print (30 == 31) #true
+print(type (30 == 31)) #mostra qual é o tipo de dado
+print(type("Teste")) #mostra qual é o tipo de dado
