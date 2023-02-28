a4eb3deb9798566668aa6ca43600a7e98d8d58cf
JoaoPNuneSilva <joaopedronunesdasilva2@gmail.com>
2023-02-27 22:17:35 -0300

    aula

--- joao.py
+++ joao.py
@@ -1,18 +1,30 @@
+nome = "João Pedro"
+sobrenome = "Nunes da Silva"
+idade = 21
+altura = 1.70
+peso = "70kg"
+maiordeidade = idade >= 18
+print (nome, sobrenome, sep=" ")
+print("Idade: " , idade)
+print("Altura: " , altura)
+print("Peso: ", peso)
+print ("É maior de idade: " , maiordeidade)
+
 #Comentário
-print("Eu sou a vingança")
-print(22, 33, 44)
-print(22, 33, 44, sep="-")
-print("Eu sou a vingança", 22, 33, 44, sep="-")
-print (30 == 30) #false
-print (30 == 31) #true
-print(type (30 == 31)) #mostra qual é o tipo de dado
-print(type("Teste")) #mostra qual é o tipo de dado
-#print(type(11, 1.1, True))
-print(int("1") + 1)
-print(float("1") + 1)
-print(type(int("1") + 1))
-nome = "string"
-idade = 2023
-print (nome)
-print (str(idade) + "anos")
-print (idade, "anos")
+# print("Eu sou a vingança")
+# print(22, 33, 44)
+# print(22, 33, 44, sep="-")
+# print("Eu sou a vingança", 22, 33, 44, sep="-")
+# print (30 == 30) #false
+# print (30 == 31) #true
+# print(type (30 == 31)) #mostra qual é o tipo de dado
+# print(type("Teste")) #mostra qual é o tipo de dado
+# #print(type(11, 1.1, True))
+# print(int("1") + 1)
+# print(float("1") + 1)
+# print(type(int("1") + 1))
+# nome = "string"
+# idade = 2023
+# print (nome)
+# print (str(idade) + "anos")
+# print (idade, "anos")
