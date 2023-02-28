0662d2e9d74c545b635c725a6f1be8d1a8e52277
JoaoPNuneSilva <joaopedronunesdasilva2@gmail.com>
2023-02-27 22:07:26 -0300

    aula

--- joao.py
+++ joao.py
@@ -7,3 +7,12 @@ print (30 == 30) #false
 print (30 == 31) #true
 print(type (30 == 31)) #mostra qual é o tipo de dado
 print(type("Teste")) #mostra qual é o tipo de dado
+#print(type(11, 1.1, True))
+print(int("1") + 1)
+print(float("1") + 1)
+print(type(int("1") + 1))
+nome = "string"
+idade = 2023
+print (nome)
+print (str(idade) + "anos")
+print (idade, "anos")
